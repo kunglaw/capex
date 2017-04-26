bd7bf9d15d02e398d57e2de1152574369eba8cea
Aries Dimas Yudhistira <alhusna901@gmail.com>
2017-04-26 19:40:46 +0700

    second changes

--- application/modules/master/models/Department_model.php
+++ application/modules/master/models/Department_model.php
@@ -4,31 +4,39 @@
 	{
 		function __construct()
 		{
-			
+
 		}
-		
+
 		function list_department()
 		{
 			$str = "SELECT * FROM department_ms ";
 			$q = $this->db->query($str);
 			$f = $q->result_array();
-			
-			return $f;	
-			
+
+			return $f;
+
 		}
-		
+
 		function detail_department($arr_department)
 		{
-			$kode_department = $arr_department["kode_department"];
-			
+
+			if(is_array($arr_department))
+			{
+				$kode_department = $arr_department["kode_department"];
+			}
+			else {
+				$kode_department = $arr_department;
+			}
+
+
 			$str = "SELECT * FROM department_ms WHERE kode_department = '$kode_department' ";
 			$q = $this->db->query($str);
 			$f = $q->row_array();
-			
-			return $f;	
-			
+
+			return $f;
+
 		}
-		
+
 		function add_department($arr_department)
 		{
 			$str = "INSERT INTO department_ms SET ";
@@ -36,41 +44,41 @@
 			$str.= "department		 = '$arr_department[department]'	  ,";
 			$str.= "kode_div		 = '$arr_department[kode_div]'		  ,";
 			$str.= "description		 = '$arr_department[description]'	   ";
-			
+
 			return $q = $this->db->query($str);
-			
+
 		}
-		
+
 		function update_department($arr_department)
 		{
 			$str  = "UPDATE department_ms SET 							   ";
-			
+
 			$str .= "department		 = '$arr_department[department]' 	  ,";
 			$str .= "kode_div		 = '$arr_department[kode_div]'		  ,";
 			$str .= "description	 = '$arr_department[description]'	   ";
 			$str .= "WHERE kode_department = '$arr_department[kode_department]' ";
-			
+
 			return $q = $this->db->query($str);
-			
-			
+
+
 		}
-		
-		
+
+
 		function delete_department($arr_department)
 		{
-			
+
 			$kode_department = $arr_department["kode_department"];
-			
+
 			$str = "DELETE FROM department_ms WHERE kode_department = '$kode_department' ";
-			
-			return $q = $this->db->query($str); 	
-			
+
+			return $q = $this->db->query($str);
+
 		}
-		
+
 		function __destruct()
 		{
-			
-			
+
+
 		}
-		
+
 	}
---
+++ application/modules/opex/views/content/update_additional.php
@@ -0,0 +1,118 @@
+<script>
+	function load_opex_trd()
+	{
+		var year = $("#year").val();
+		var opex_account = $("#opex_account").val();
+		var month = $("#month").val();
+		
+		$.ajax({
+			
+			type:"POST",
+			url:"<?=base_url("opex/json_ajax/load_opex_trd")?>",
+			data:"year="+year+"&opex_account="+opex_account+"&month="+month,
+			dataType:"JSON",
+			success: function(data)
+			{
+				$("#opex_trd_id").val(data.opex_trd_id);
+				
+			}
+			
+		});
+		
+	}
+	
+	load_opex_trd();
+
+</script>
+<div class="modal inmodal" id="update-additional-modal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content animated bounceInRight">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                
+                <h4 class="modal-title">update Additional Detail Opex</h4>
+                
+            </div>
+            <form class="form" id="update-additional-form" onSubmit="">
+              <div class="modal-body">
+              	<div id="additional-info"></div>
+              	<input type="hidden" name="year" id="year" value="<?=$year?>" >
+                <input type="hidden" name="kode_department" id="kode_department" value="<?=$this->session->userdata("kode")?>">
+              	<input type="hidden" name="opex_trd_id" id="opex_trd_id" value="" >
+                
+                <div class="form-group">
+                	<label> Opex Account </label>
+                    <select name="opex_account" id="opex_account" class="form-control">
+                      <?php foreach($opex_acc as $row){ ?>	
+                        <option value="<?=$row["no_acc_opex"]?>"> <?=$row["account_name"]?> | <?=$row["detail"]?></option>
+                      <?php } ?>
+                    </select>
+                </div>
+                
+                <span class="clearfix"></span>
+                <div class="row">
+                  <div class="form-group col-md-5">
+                      <label> Month </label>
+                      <select name="month" id="month" class="form-control " >
+                         <?php foreach($months as $key=>$val){ ?>
+                          <option value="<?=$val?>"><?=$val?></option>
+                         <?php } ?>
+                      </select>
+                  </div>
+                 
+                  <div class="form-group col-md-5">
+                      <label> Amount Budget </label>
+                      <input type="text" name="budget" value="" class="form-control">
+                  </div>
+                </div>
+                 <span class="clearfix"></span>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Save changes</button>
+              </div>
+            </form>
+        </div>
+  </div>
+</div>
+
+<script>
+	$("#update-additional-modal").modal({
+		
+		show:true	
+		
+	});
+	
+	$("form#update-additional-form").submit(function(){
+		
+		$.ajax({
+			
+			type:"POST",
+			dataType:"json",
+			data:$("#update-additional-form").serialize(),
+			
+			url:"<?=base_url("opex/additional/update_additional_process")?>",
+			success: function(dt)
+			{
+				
+				$("#additional-info").html(dt.message);
+				
+			}
+		});
+		
+		return false;
+	});
+	
+	$("#month").change(function(){
+		
+		load_opex_trd();	
+	
+	});
+	
+	$("#opex_account").change(function(){
+		
+		load_opex_trd();	
+	})
+
+
+</script>
